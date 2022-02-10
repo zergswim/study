b91cc742bfc869b3be4f9cac60b6de91da322a69
zergswim <pmg304@hanmail.net>
2022-02-10 19:14:30 +0900

    test_simple

---
+++ Assets/PlayerBall.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBall : MonoBehaviour
+{
+    public float JumpPower;
+    Rigidbody rigid;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+            rigid.AddForce(new Vector3(0, JumpPower, 0), ForceMode.Impulse);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log(Camera.main.pixelWidth/2-Input.mousePosition.x);
+
+            if(Camera.main.pixelWidth/2-Input.mousePosition.x > 0)
+                rigid.AddForce(new Vector3(10, JumpPower, 0), ForceMode.Impulse);
+            else
+                rigid.AddForce(new Vector3(-10, JumpPower, 0), ForceMode.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        rigid.AddForce(new Vector3(h, 0, v), ForceMode.Impulse);
+    }
+}
